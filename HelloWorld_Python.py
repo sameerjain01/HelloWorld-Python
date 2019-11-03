eec886c70251b8d86c79ea258725b5f9bcd55291
Sameer <sameejain01@outlook.com>
2019-11-03 09:47:39 -0600

    some fun with strings
    having some fun with hello world

--- HelloWorld_Python.py
+++ HelloWorld_Python.py
@@ -2,4 +2,14 @@
 print("Hello World! I am c# developer learning Python")
 print("Please enter name: ")
 entered_name = input()
-print("You entered: " + entered_name)
+print("You entered: " + entered_name + ". I used + to concetenate string")
+print("You entered:", entered_name ,".","I used commas to join string")
+print("job".lower().startswith("J".lower()))
+print("10003".isdigit())
+
+fav_color = input("what is your favroite color? ")
+print(entered_name+"'s favorite color is", fav_color)
+
+string_statement ="The quick brown fox jumps over the lazy dog"
+print("brown" in string_statement)
+
